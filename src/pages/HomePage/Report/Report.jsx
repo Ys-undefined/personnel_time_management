dadf6ed30609631e17bcc0c106eef9dbb1b5bc2c
“huanghe1997” <1959974025@qq.com>
2023-05-13 16:54:06 +0800

    ‘修改框架样式’

--- src/pages/HomePage/Report/Report.jsx
+++ src/pages/HomePage/Report/Report.jsx
@@ -1,6 +1,6 @@
 import {Layout} from 'antd';
 const {Sider, Content } = Layout;
-import styles from './record.module.scss'
+import styles from '../../HomePage/general.module.scss'
 import SideNavi from '../../../components/SideNavi/SideNavi.jsx'
 import {Outlet, useLocation} from 'react-router-dom'
 
@@ -19,10 +19,10 @@ export const Report = () => {
     const location = useLocation()
     return (
         <>
-            <Sider className={styles.siderStyle}>
+            <Sider className={styles.siderStyle} >
                 <SideNavi items={items} naviUrl={location.pathname} defaultDisplayKey='/daily-report'/>
             </Sider>
-            <Content className={styles.contentStyle}>
+            <Content className={styles.contentStyle} >
                 <Outlet/>
             </Content>
         </>
