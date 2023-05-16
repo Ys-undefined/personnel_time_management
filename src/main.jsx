f10247c68d851bb0e6b1cc62b1d91606d3a9da33
“huanghe1997” <1959974025@qq.com>
2023-05-16 22:11:52 +0800

    ss

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,9 @@ import './assets/styles/global.scss'
 import App from './App.jsx'
 
 import {BrowserRouter} from 'react-router-dom'
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
             <App/>
-        </DevSupport>
     </BrowserRouter>
 )
